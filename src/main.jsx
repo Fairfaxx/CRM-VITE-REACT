17ca1cb39b91290eaa8b97da60d7af461b04572b
FefeLemaire <federico.lemaire@alloy.digital>
2023-01-16 16:35:30 -0300

    first layout added and reat-router-dom

---
+++ src/main.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import './index.css';
+
+import { Layout } from './components/Layout';
+import { NewClient } from './pages/NewClient';
+import { Index } from './pages/Index';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <Index />,
+      },
+      {
+        path: '/clients/new',
+        element: <NewClient />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
---
+++ src/pages/NewClient.jsx
@@ -0,0 +1,3 @@
+export const NewClient = () => {
+  return <div className="text-6xl">NewClient</div>;
+};
