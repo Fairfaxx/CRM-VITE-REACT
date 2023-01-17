c8f218112e6a7930f3962962deda318d9cdeafab
FefeLemaire <federico.lemaire@alloy.digital>
2023-01-17 16:06:45 -0300

    form validation and useActionData checking for errors

---
+++ src/components/Errors.jsx
@@ -0,0 +1,7 @@
+export const Errors = ({ children }) => {
+  return (
+    <div className="text-center my-4 bg-red-700 text-white font-bold uppercase p-3">
+      {children}
+    </div>
+  );
+};
--- src/pages/NewClient.jsx
+++ src/pages/NewClient.jsx
@@ -1,16 +1,36 @@
-import { useNavigate, Form } from 'react-router-dom';
+import { useNavigate, Form, useActionData } from 'react-router-dom';
+import { Errors } from '../components/Errors';
 import UserForm from '../components/UserForm';
 
 export const action = async ({ request }) => {
   const formData = await request.formData();
 
   // A way to extract the form data
-  // const data = Object.fromEntries(formData);
-  // console.log(data);
+  const data = Object.fromEntries(formData);
+
+  const email = formData.get('email');
+  //Validate
+  const errors = [];
+
+  if (Object.values(data).includes('')) {
+    errors.push('All fields are required');
+  }
+
+  let regex = new RegExp(
+    "([!#-'*+/-9=?A-Z^-~-]+(.[!#-'*+/-9=?A-Z^-~-]+)*|\"([]!#-[^-~ \t]|(\\[\t -~]))+\")@([!#-'*+/-9=?A-Z^-~-]+(.[!#-'*+/-9=?A-Z^-~-]+)*|[[\t -Z^-~]*])"
+  );
+
+  if (!regex.test(email)) {
+    errors.push('The email address is not a valid email address');
+  }
+  // Return errors if there are any
+  if (Object.keys(errors).length) return errors;
 };
 
 export const NewClient = () => {
   const navigate = useNavigate();
+  const errors = useActionData();
+
   return (
     <>
       <h1 className="font-black text-4xl text-blue-900">New Customer</h1>
@@ -26,7 +46,8 @@ export const NewClient = () => {
       </div>
 
       <div className="bg-white shadow rounded-md md:w-3/4 mx-auto px-5 py-10 mt-20">
-        <Form method="post">
+        {errors?.length && errors.map((e, i) => <Errors key={i}>{e}</Errors>)}
+        <Form method="post" noValidate>
           <UserForm />
           <input
             type="submit"
